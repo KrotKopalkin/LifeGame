c6d1ac78c59c7d908cf9a4ee16a2ae28ff95eb5f
Katel212 <ka.kovjarova@gmail.com>
2019-12-19 16:47:08 +0300

    Добавлена процедура вывода клеток

---
+++ lifegame.pas
@@ -0,0 +1,14 @@
+﻿procedure printcells(a: array [,] of boolean);
+begin
+  for var i := 0 to a.GetLength(0) - 1 do
+  begin
+    for var j := 0 to a.GetLength(1) - 1 do
+      if a[i, j] then write('1')
+      else write(0);
+    writeln();
+  end;
+end;
+
+begin
+ 
+end.
