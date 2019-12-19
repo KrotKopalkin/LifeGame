d0fbf5f5cc29a2ddcb1f3c17eb7ea66427b3d156
Николаев Евгений Николаевич <evnikolaev@sfedu.ru>
2019-12-19 17:13:11 +0300

    count-cell

--- lifegame.pas
+++ lifegame.pas
@@ -1,14 +1,14 @@
-﻿procedure printcells(a: array [,] of boolean);
+﻿function Kolvo(var a: array [,] of boolean; x, y: integer): integer;
 begin
-  for var i := 0 to a.GetLength(0) - 1 do
-  begin
-    for var j := 0 to a.GetLength(1) - 1 do
-      if a[i, j] then write('1')
-      else write('0');
-    writeln();
-  end;
+  for var i := -1 to 1 do
+    for var j := -1 to 1 do
+    begin
+      
+      if a[(x+i) mod a.getLength(0),(y+j) mod a.getLength(1)] then
+      result += 1;
+    end;
 end;
 
 begin
- 
+  
 end.
