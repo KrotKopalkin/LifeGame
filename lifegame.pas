b0a802e1e35fcaaf9f179226266bdf0db9997e9a
Недоруб Артем Александрович <anedorub@sfedu.ru>
2019-12-19 17:05:49 +0300

    Interat

---
+++ lifegame.pas
@@ -0,0 +1,21 @@
+﻿procedure iterat(var a: array [,] of boolean);
+begin
+  for var i := 0 to a.RowCount - 1 do
+    for var j := 0 to a.ColCount - 1 do
+    begin
+      var nei := kolvo(a, i, j);
+      if(not a[i, j]) then
+      begin
+        if nie = 3 then
+          a[i, j] := true;
+      end
+      else 
+        if nie <> 2 and nie <> 3 then
+          a[i, j] := false;
+    end;
+  
+end;
+
+begin
+  
+end.
